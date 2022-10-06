683a58e2d859d9ce13396413ab584140e3454075
williamhCode <83525937+williamhCode@users.noreply.github.com>
2022-10-05 22:24:25 -0400

    move to correct location + bug fixes

--- Assets/Scripts/PlayerSystems/Stamina.cs
+++ Assets/Scripts/PlayerSystems/Stamina.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Stamina : MonoBehaviour
 {
+    private int stamina;
     // Start is called before the first frame update
     void Start()
     {
-        
+        stamina = 100;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void setStamina(int s) {
+        stamina = s;
+        if (stamina > 100) {
+            stamina = 100;
+        }
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void addStamina(int s) {
+        stamina += s;
+        if (stamina > 100) {
+            stamina = 100;
+        }
+    }
+    
+    public void subtractStamia(int s) {
+        stamina -= s;
+        if (stamina < 0) {
+            stamina = 0;
+        }
     }
 }
