e2db19e4f0694aa9973a97928e276e50d7bca030
Bryce L <bryce.liebenauer@yahoo.com>
2022-10-12 21:25:39 -0400

    Open Door / Possible Local Saving Error
    I added the OpenDoor function subtract stamina to open doors, but also it appears there was an error in saving my code from the previous commit, so all of the other changes maybe duplicates from the previous commit, I am not totally sure.

--- Assets/Scripts/PlayerStamina.cs
+++ Assets/Scripts/PlayerStamina.cs
@@ -12,36 +12,43 @@ public class PlayerStamina : MonoBehaviour
 
     private bool isClimbing; //A boolean to see if the player is climbing
 
-    private int framesClimbed; //An integer to keep track of the number of frames the user has climbed, used to calculate stamina loss
+    private float timeClimbed; //A float to keep track of the number of seconds the user has climbed, used to calculate stamina loss
 
-    // Start is called before the first frame update. As of 10/7/22, 100 is an arbitrary number
+    // Start is called before the first frame update. As of 10/7/22, 100 is an arbitrary number, and it resets the timeClimbed to 0.0
     void Start()
     {
         stamina = 100;
+        timeClimbed = 0.0f;
     }
 
     /* Update is called once per frame. As of 10/5/22, stamina won't regen when standing still. Could change in future.
        
-       The climbing portion means every 6 frames of climbing, a person loses one stamina, or loss of 10 stamina per second of climbing.
-       As of 10/7/22, this number if arbitrary. The sound / animation comments are only in there as reminders, they could go somewhere else
-    */
+       The climbing portion means every 6 frames of of climbing, or every 0.1 seconds, a person loses one stamina, or loss of 10 stamina per second of climbing.
+       As of 10/7/22, this number if arbitrary. The sound / animation comments are only in there as reminders, they could go somewhere else. The subtraction
+       of 0.1 from timeClimbed allows the time to always stay between 0.0 and 0.1, while also allowing to get to 0.1, change the stamina, then reset. If I 
+       didn't reset it, then time would be equal to the number of seconds a user has been climbing, while can be a big number after some playtime, 
+       which isn't what we want. Dm Bryce L. on Discord if more of an explination is needed
+       */
 
     void Update()
     {
-        while(isClimbing) {
-           framesClimbed++;
-           if(framesClimbed % 6 == 0) {
-            stamina -= 1;
-            //play climbing sound here
-            //play climbing animation here
-           }
+        //6*Time.deltaTime
+        while (isClimbing) {
+            timeClimbed += Time.deltaTime;
+            if (timeClimbed % 0.1 == 0) {
+                stamina -= 1;
+                timeClimbed -= 0.1f;
+                //call climbing animation here
+                //call climbing sound here
+            }
         }
+        
     }
 
     /*
-       Dashes, plays sound / animation, and subtracts stamina for dash, if the player has stamina for it. If  As of 10/7/22, 20 is an arbitrary number
+       Dashes, plays sound / animation, and subtracts stamina for dash, if the player has stamina for it. As of 10/7/22, 20 is an arbitrary number
     */
-    void Dash() {
+    public void Dash() {
         
         if (stamina >= 20) {
         //play dash animation here
@@ -53,7 +60,7 @@ public class PlayerStamina : MonoBehaviour
     /*
        Heavy attacks, plays sound / animation, and subtracts stamina for attack, if the player has stamina for it. As of 10/7/22, 25 is an arbitrary number
     */
-    void HeavyAttack() {
+    public void HeavyAttack() {
         if (stamina >= 25) {
             //play heavy attack animation here
             //play heaavy attack sound here
@@ -61,11 +68,22 @@ public class PlayerStamina : MonoBehaviour
             }
     }
 
+     /*
+       Opens a door using stamina, plays the associated sound and animation. As of 10/12/22, 15 is an arbitrary number
+     */
+    public void OpenDoor() {
+        if (stamina >= 15) {
+            //play open door animation here
+            //play open door sound here
+            stamina -= 15;
+        }
+    }
+
     /*
       Changes stamina using a parameter. Used for items that buff / increase stamina, or decrease it for other bonuses if an item like
       that exists. Needs reasonable limits
     */
-    void AddStamina(int stamina) {
+    public void AddStamina(int stamina) {
          this.stamina += stamina;
          if (this.stamina >= 100) {
             this.stamina = 100;
@@ -73,15 +91,11 @@ public class PlayerStamina : MonoBehaviour
          
     }
 
+
     /*
-      This is used to change the boolean that keeps track of if the user is climbing. Every time the user starts to climb or stops climbing, the
-      number of frames climbed resets to 0. This ensures that stamina is subtracted properly. For example, if the user starts climbing while the
-      remainder when framesClimbed is divided by 6 is 5(or framesClimbed % 6 = 5), they will instantly lose stamina on the next frame of climbing, which
-      isn't what we want if we plan on having the player lose stamina every 6 frames of climbing. This setting it to 0 every time the isClimbing boolean
-      is changed fixes that error. For an more in depth explination, dm Bryce L. on discord
+      This is used to change the boolean that keeps track of if the user is climbing. 
     */
-    void ChangeIsClimbing(bool isClimbing) {
+    public void ChangeIsClimbing(bool isClimbing) {
         this.isClimbing = isClimbing;
-        framesClimbed = 0;
     }
 }
