7f190986662c96e13985ce5dc6c9fe7269a1f3bf
Kyle <45471789+kphillips2003@users.noreply.github.com>
2022-10-26 21:26:48 -0400

    different item types
    Don't have a field in the item class to determine for the methods, so the methods don't work yet

--- Assets/Scripts/PlayerSystems/InventorySystem/InventorySystem.cs
+++ Assets/Scripts/PlayerSystems/InventorySystem/InventorySystem.cs
@@ -3,20 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class InventorySystem : /*ScriptableObject*/MonoBehaviour {
+public class InventorySystem : MonoBehaviour {
 
     // [[[NOTE]]] - assumes the following:
         // player has a non-trigger Collider on a gameobject with the "Player" tag
         // the Canvas component on the InventorySystem has the main camera assigned to it (for 3D item display)
         // This class will be accessed with InventorySystem.instance
 
-
     // CONTROLS -
         // drop selected item - 'Q'
         // use selected item - 'E'
         // pickup item - touch it with player *see above note*
-
-
 
     // The static instance used to use this class
     public static InventorySystem instance;
@@ -24,11 +21,18 @@ public class InventorySystem : /*ScriptableObject*/MonoBehaviour {
     // The height at which items are dropped at before falling toward the ground
     public const float ITEM_DROP_HEIGHT = 1f;
 
-    private static int MAX_SLOTS = 3;
+    private static int NUMBER_OF_GENERAL_SLOTS = 3;
+    private static int MAX_STACKS = 3;
+    private static int NUMBER_OF_KEY_SLOTS = 1;
+    private static int NUMBER_OF_TOOL_SLOTS = 1;
+    private static int INDEX_OF_KEY = NUMBER_OF_GENERAL_SLOTS;
+    private static int INDEX_OF_TOOL = NUMBER_OF_GENERAL_SLOTS + NUMBER_OF_KEY_SLOTS;
+    private static int MAX_SLOTS = NUMBER_OF_GENERAL_SLOTS + NUMBER_OF_KEY_SLOTS + NUMBER_OF_TOOL_SLOTS;
 
     [SerializeField] private GameObject slotHolder;
 
     private InventorySlot[] inventory = new InventorySlot[MAX_SLOTS];
+    private int[] itemsPerSlot = new int[MAX_SLOTS];
 
     // the index of the current selected slot
     private int selectedSlotNum = 0;
@@ -38,7 +42,6 @@ public class InventorySystem : /*ScriptableObject*/MonoBehaviour {
 
         // Grabs all children InventorySlots to base the inventory array on the InventoryCanvas prefab
         inventory = slotHolder.GetComponentsInChildren<InventorySlot>();
-        MAX_SLOTS = inventory.Length;
     }
 
     // Start is called before the first frame update
@@ -78,18 +81,47 @@ public class InventorySystem : /*ScriptableObject*/MonoBehaviour {
         }
     }
 
-    private int CalculateEmptySlots() {
+    private int CalculateEmptyGeneralSlots() {
         int slots = 0;
-        for (int i = 0; i < MAX_SLOTS; i++) {
-            if (inventory[i].getHeldItem() == null) {
+        for (int i = 0; i < INDEX_OF_KEY; i++) {
+            if (inventory[i] == null) {
                 slots++;
             }
         }
         return slots;
     }
 
+    private int CalculateEmptyKeySlots() {
+        int slots = 0;
+        for (int i = INDEX_OF_KEY; i < INDEX_OF_TOOL; i++) {
+            if (inventory[i] == null) {
+                slots++;
+            }
+        }
+        return slots;
+    }
+
+    private int CalculateEmptyToolSlots() {
+        int slots = 0;
+        for (int i = INDEX_OF_TOOL; i < MAX_SLOTS; i++) {
+            if (inventory[i] == null) {
+                slots++;
+            }
+        }
+        return slots;
+    }
+
+    private int FindIndexOf(Item item) {
+        for (int i = 0; i < MAX_SLOTS; i++) {
+            if (inventory[i] == item) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     void Display() {
-        
+        //I don't know if we need this method or how to imp it
     }
 
     /// <summary>
@@ -99,7 +131,7 @@ public class InventorySystem : /*ScriptableObject*/MonoBehaviour {
     /// <param name="item"> The Item script of the item GameObject that should be picked up. </param>
     /// <returns>True if there was room to pick up the item</returns>
     public void Add(Item item) {
-        int slot = CalculateEmptySlots();
+        int slot = CalculateEmptyGeneralSlots();
         if (slot == 0) {
             Choose(item);
         }
@@ -124,6 +156,60 @@ public class InventorySystem : /*ScriptableObject*/MonoBehaviour {
         }
     }
 
+    void Add(Item item, string anotherVar) { //Second var is so it doesn't complain
+        int slots = -1;
+        string type = ""; //Empty String so the code doesn't error before we have a method to determine what type of item it is
+        // string type = item.GetItemType();
+
+        if (type.Equals("General")) {
+            slots = CalculateEmptyGeneralSlots();
+            for (int i = 0; i < INDEX_OF_KEY; i++) {
+                if (inventory[i].Equals(item)) {
+                    if (item.isStackable()) { //Need a way to check if it is stackable
+                        if (itemsPerSlot[i] < MAX_STACKS) {
+                            itemsPerSlot[i]++;
+                            return;
+                        }
+                    }
+                }
+            }
+
+            if (slots != 0) {
+                for (int i = 0; i < INDEX_OF_KEY; i++) {
+                    if (inventory[i] ==  null) {
+                        inventory[i] = item;
+                        itemsPerSlot[i] = 1;
+                        break;
+                    }
+                }
+            }
+
+            Choose(item);
+        } else if (type.Equals("Key")) { // Assumes that keys aren't stackable
+            slots = CalculateEmptyKeySlots();
+            if (slots != 0) {
+                for (int i = INDEX_OF_KEY; i < INDEX_OF_TOOL; i++) {
+                    if (inventory[i] == null) {
+                        inventory[i] = item;
+                        return;
+                    }
+                }
+            }
+
+            Choose(item);
+        } else if (type.Equals("Tool")) { // Assumes that tools are not stackable
+            for (int i = INDEX_OF_TOOL; i < MAX_SLOTS; i++) {
+                if (inventory[i] == null) {
+                    inventory[i] = item;
+                    return;
+                }
+            }
+
+            Choose(item);
+        }
+
+    }
+
     /// <summary>
     /// Uses the item in the currently selected inventory slot.  Then, the item is destroyed and removed from the inventory.
     /// </summary>
@@ -132,10 +218,16 @@ public class InventorySystem : /*ScriptableObject*/MonoBehaviour {
 
         if (curItem != null)
         {
+            int index = FindIndexOf(curItem);
+            int stacks = itemsPerSlot[index];
             curItem.Use();
 
             // drops item
-            Drop();
+            if (stacks <= 1) {
+                Drop();
+            } else {
+                itemsPerSlot[index]--;
+            }
 
             // destroys dropped item
             curItem.DestroyItem();
@@ -157,8 +249,49 @@ public class InventorySystem : /*ScriptableObject*/MonoBehaviour {
     }
 
     void Choose(Item item) {
-        // select which item you want to drop to trade out this item instead
+        //Choose when the inventory is full
+        //I dont know how to implement at this moment [Just brainstorming here]
+        //Open the inventory 
+        //Display what item do you want to drop
+        //Key press to not pick up the item
+        //If they want to keep the item, they select the item that they want to drop
+        //Press a key if they want to drop the currently selected item
+
+        // Q to drop new item
+        // R to drop selected item to replace it
+
+        string type = "";
+        // string type = item.GetItemType();
+
+        while (true) {
+            if (type.Equals("General")) {
+                if (Keyboard.current.qKey.wasPressedThisFrame) {
+                    return;
+                } else if (Keyboard.current.rKey.wasPressedThisFrame) {
+                    Drop();
+                    Add(item, ""); //Get rid of string when the version of Add() is selected
+                    return;
+                }
+            } else if (type.Equals("Key")) {
+                if (Keyboard.current.qKey.wasPressedThisFrame) {
+                    return;
+                } else if (Keyboard.current.rKey.wasPressedThisFrame) {
+                    Drop();
+                    Add(item, ""); //Get rid of string when the version of Add() is selected
+                    return;   
+                }
+            } else if (type.Equals("Tool")) {
+                if (Keyboard.current.qKey.wasPressedThisFrame) {
+                    return;
+                } else if (Keyboard.current.rKey.wasPressedThisFrame) {
+                    Drop();
+                    Add(item, ""); //Get rid of string when the version of Add() is selected
+                    return;
+                }
+            }
+        }
     }
+
 
     /// <summary>
     /// [temporary] This function registers Add() to every Item's OnGrab() event.
