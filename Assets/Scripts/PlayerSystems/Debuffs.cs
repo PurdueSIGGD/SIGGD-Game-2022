8c489289f61ae75ab02f7347235bd8b1a615e6b4
Bryce L <bryce.liebenauer@yahoo.com>
2022-10-26 20:53:51 -0400

    Debuffs to Debuff
    Changed class name from Debuffs to Debuff to reflect the idea that it will now be a super class for all of the individual debuff classes

--- Assets/Scripts/PlayerSystems/Debuffs.cs
+++ Assets/Scripts/PlayerSystems/Debuffs.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Debuffs : MonoBehaviour
+public class Debuff : MonoBehaviour
 {
     private Movement movement;
 
