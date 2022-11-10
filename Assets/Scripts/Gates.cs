1d61dc90efe5af08b42693c7194dd6307dde3611
iyang2006 <yang2006@purdue.edu>
2022-11-09 21:49:51 -0500

    setup

---
+++ Assets/Scripts/Gates.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gates : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Opens an interactable object if possible
+    // Returns true if it can be opened, false if locked
+    bool open()
+    {
+
+    }
+}
