e05be6246c7a7df338ef8fa049ab2e3e2dbca71e
iyang2006 <yang2006@purdue.edu>
2022-11-09 23:06:10 -0500

    Description

--- Assets/Scripts/Gates.cs
+++ Assets/Scripts/Gates.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Gates : MonoBehaviour
 {
-    
+    /* Includes functions for interactable, non-item objects such as
+     * doors, chests, or levers
+     */
 
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Gates/GatesObj.cs
+++ Assets/Scripts/Gates/GatesObj.cs
@@ -4,5 +4,7 @@ using UnityEngine;
 
 public class GatesObj : MonoBehaviour
 {
-    
+    /* Creates gate objects (interactable, non-item objects) such as
+     * doors, chests, or levers
+     */
 }
