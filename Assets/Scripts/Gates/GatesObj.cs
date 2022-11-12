ad02d28537ddd7d12672f3671da2359dc9eabf1b
iyang2006 <yang2006@purdue.edu>
2022-11-11 20:01:49 -0500

    gate obj

--- Assets/Scripts/Gates/GatesObj.cs
+++ Assets/Scripts/Gates/GatesObj.cs
@@ -2,9 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GatesObj : MonoBehaviour
-{
-    /* Creates gate objects (interactable, non-item objects) such as
+/* Creates gate objects (interactable, non-item objects) such as
      * doors, chests, or levers
      */
+
+public class GatesObj : MonoBehaviour
+{
+    // Fields
+    private bool locked = false;
+    private bool keyNeeded = false;
+    private bool passwordNeeded = false;
+    // private Item item 
+
+    public bool isLocked() { return locked; }
+    public void setLocked(bool var) { locked = var; }
+    public bool isKeyNeeded() { return keyNeeded; }
+    public void setKeyNeeded(bool var) { keyNeeded = var; }
+    public bool isPasswordNeeded() { return passwordNeeded; }
+    public void setPasswordNeeded(bool var) { passwordNeeded = var; }
 }
