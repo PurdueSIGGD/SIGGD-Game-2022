4dea9de1810c60e5cc31fabed69bde5bc8623d65
iyang2006 <yang2006@purdue.edu>
2022-11-16 21:43:37 -0500

    gate stuff

--- Assets/Scripts/Gates/GatesObj.cs
+++ Assets/Scripts/Gates/GatesObj.cs
@@ -12,12 +12,34 @@ public class GatesObj : MonoBehaviour
     private bool locked = false;
     private bool keyNeeded = false;
     private bool passwordNeeded = false;
-    // private Item item 
+    public Item key;
+    private int identity = -1;
 
+    // Constructor
+    public GatesObj()
+    {
+        locked = false;
+        keyNeeded = false;
+        passwordNeeded = false;
+        identity = -1;
+    }
+
+
+    // Methods
     public bool isLocked() { return locked; }
     public void setLocked(bool var) { locked = var; }
     public bool isKeyNeeded() { return keyNeeded; }
     public void setKeyNeeded(bool var) { keyNeeded = var; }
     public bool isPasswordNeeded() { return passwordNeeded; }
     public void setPasswordNeeded(bool var) { passwordNeeded = var; }
+    public int getIdentity() { return identity; }
+    public void setIdeneity(int var) { identity = var;  }
+
+
+    public bool openObj(Item key)
+    {
+        if (!key.isType(ItemType.KEY)) return false;
+        //if (key == identity) return true;
+        return false;
+    }
 }
