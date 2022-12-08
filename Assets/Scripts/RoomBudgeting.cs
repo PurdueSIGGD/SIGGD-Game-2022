f307f8d09946b8afeda5168fbc0a83bd4030a5dc
Jared Brown <57924789+JaredTheBro@users.noreply.github.com>
2022-12-07 22:07:31 -0500

    Room objects actually spawn in now

--- Assets/Scripts/RoomBudgeting.cs
+++ Assets/Scripts/RoomBudgeting.cs
@@ -43,41 +43,13 @@ public class RoomBudgeting : MonoBehaviour {
         spawns = new List<GameObject>();
         Transform[] roomObjTransforms = transform.GetComponentsInChildren<Transform>(false);
         foreach (Transform t in roomObjTransforms) {
-            if (t.gameObject.tag.Equals("ObjectSpawn")) {
+            if (t.gameObject.tag.Equals("RoomSpawn")) {
                 spawns.Add(t.gameObject);
             }
         }
 
         // The objects' transforms to be allowed to spawn in the room should be added to the script's list through the serialized field in the editor.
         List<Transform> toSpawn = new List<Transform>();
-
-        /* Version 2: Same as version 1, but when an object whose weight exceeds the remaining budget is chosen, the loop doesn't stop.
-        Instead, it doesn't modify the remaining budget for that iteration and randomly chooses another object that is NOT the one that
-        was just chosen. It keeps doing this, ignoring each object from the list that has too much weight, until an object is chosen that
-        fits in the remaining budget, which is spawned. This process repeats until the point that no object in the list will fit in the budget.
-        // Implement version 2 here. It'll likely need a second list that holds references to the ignored objects or incedes to exclude them
-        // from the random selection somehow.
-        */
-        /*
-        while (spawnables.Count != 0 /* && toSpawn.Count < spawns.Count *//* ) {
-            int chosenIndex = (int) Random.Range(0, spawnables.Count);
-            int objWeight = spawnables[chosenIndex].gameObject.GetComponent<Attributes>().weight;
-            if (objWeight <= tempBudget) {
-                toSpawn.Add(spawnables[chosenIndex]);
-                tempBudget -= objWeight;
-            } else {
-                spawnables.Remove(spawnables[chosenIndex]);
-            }
-        }
-        string test = "With a budget of " + budget + ", spawned these objects: ";
-        foreach (Transform t in toSpawn) {
-            test += t.name + " (" + t.GetComponent<Attributes>().weight + "), ";
-        }
-        test += "with " + tempBudget + " budget left over.";
-
-        Debug.Log(test);
-        */
-
 
         /* Version 3: Add the ability for devs to set constraints on how many objects can spawn in a room. Have a minimum and maximum
         associated with that room through more lists serialized from this script, or making use of a Map-type list to pair the mins and
@@ -94,9 +66,9 @@ public class RoomBudgeting : MonoBehaviour {
                 i--;
             }
         }
-        while (spawnables.Count > 0 /* && toSpawn.Count < spawns.Count */ ) {
+        while (spawnables.Count > 0 && toSpawn.Count < spawns.Count) {
             for (int i = 0; i < spawnables.Count; i++) {
-                while (minimums[i] > numSpawned[i]) {
+                while (minimums[i] > numSpawned[i] && toSpawn.Count < spawns.Count) {
                     if (spawnables[i].gameObject.GetComponent<Attributes>().weight <= tempBudget) {
                         toSpawn.Add(spawnables[i]);
                         tempBudget -= spawnables[i].gameObject.GetComponent<Attributes>().weight;
@@ -132,7 +104,6 @@ public class RoomBudgeting : MonoBehaviour {
 
         Debug.Log(test);
 
-
         /* Original object weights:
             Easy Enemy - 6
             Medium Enemy - 13
@@ -143,11 +114,7 @@ public class RoomBudgeting : MonoBehaviour {
             Stamina Refill - 10
         */
 
-        foreach (Transform t in toSpawn) {
-            GameObject.Instantiate(t.gameObject);
-            // Set position and stuff here and put it into the actual room in the scene
-        }
-        
+        spawnObjects(toSpawn, spawns);
     }
 
     /// <summary>
@@ -195,6 +162,42 @@ public class RoomBudgeting : MonoBehaviour {
         maximums.RemoveAt(index);
         numSpawned.RemoveAt(index);
         minimums.RemoveAt(index);
+    }
+
+    /// <summary>
+    /// Spawns the chosen objects at the available spawnpoints, making sure not to spawn them in the floor but instead sitting
+    /// on the floor, depending on their collider. (Spawnpoints must be level with the floor)
+    /// </summary>
+    /// <param name="toSpawn"></param>
+    /// <param name="spawns"></param>
+    private void spawnObjects(List<Transform> toSpawn, List<GameObject> spawns) {
+        for (int i = 0; i < toSpawn.Count; i++) {
+            GameObject spawnedObj = GameObject.Instantiate(toSpawn[i].gameObject);
+            if (spawnedObj.transform.GetComponent<Collider>() is SphereCollider) {
+                spawnedObj.transform.position = new Vector3(
+                    spawns[i].transform.position.x,
+                    spawns[i].transform.position.y + (toSpawn[i].transform.localScale.y *
+                        (toSpawn[i].transform.GetComponent<SphereCollider>().radius)),
+                    spawns[i].transform.position.z);
+                print("Object spawned");
+            } else if (spawnedObj.transform.GetComponent<Collider>() is BoxCollider) {
+                spawnedObj.transform.position = new Vector3(
+                    spawns[i].transform.position.x,
+                    spawns[i].transform.position.y + (toSpawn[i].transform.localScale.y *
+                        (toSpawn[i].transform.GetComponent<BoxCollider>().size.y / 2)),
+                    spawns[i].transform.position.z);
+                print("Object spawned");
+            } else if (spawnedObj.transform.GetComponent<Collider>() is CapsuleCollider) {
+                spawnedObj.transform.position = new Vector3(
+                    spawns[i].transform.position.x,
+                    spawns[i].transform.position.y + (toSpawn[i].transform.localScale.y *
+                        (toSpawn[i].transform.GetComponent<CapsuleCollider>().height / 2)),
+                    spawns[i].transform.position.z);
+                print("Object spawned");
+            } else {
+                print("Error spawning object because its collider can't be handled.");
+            }
+        }
     }
     
 }
