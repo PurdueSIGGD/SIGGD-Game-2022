da683f7aa38603aafa27789d76cf2fe25c5414a1
andyjsharpe <ajsusa1@gmail.com>
2023-02-20 17:52:38 -0500

    Enemy Audio

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class AudioManager : MonoBehaviour
 {
@@ -7,7 +8,15 @@ public class AudioManager : MonoBehaviour
     [SerializeField]
     private float distPerStep;
     private float stepMeter = 0;
-    
+    [SerializeField]
+    private GameObject sightedSound;
+    [SerializeField]
+    private GameObject stunSound;
+    [SerializeField]
+    private GameObject aggroSound;
+    [SerializeField]
+    private GameObject deaggroSound;
+
     //used to calculate and play footsteps, intended to be called every frame of movement with the current speed
     public void tryStep(float ammount) //ammount is the speed moved at the time
     {
@@ -17,5 +26,25 @@ public class AudioManager : MonoBehaviour
             Instantiate(footsteps[Random.Range(0, footsteps.Length)], transform.position, Quaternion.identity);
             stepMeter -= distPerStep;
         }
+    }
+
+    public void playAggro()
+    {
+        Instantiate(aggroSound, transform.position, Quaternion.identity);
+    }
+
+    public void playDeAggro()
+    {
+        Instantiate(deaggroSound, transform.position, Quaternion.identity);
+    }
+
+    public void playStun()
+    {
+        Instantiate(stunSound, transform.position, Quaternion.identity);
+    }
+
+    public void playSighted()
+    {
+        Instantiate(sightedSound, transform.position, Quaternion.identity);
     }
 }
