86409b6d3b05c1c36d02adbd7cac0373e34da28c
andyjsharpe <ajsusa1@gmail.com>
2023-03-31 19:16:31 -0400

    Marionette anims

--- Assets/Scripts/NavmeshBaking/BakeLevelNav.cs
+++ Assets/Scripts/NavmeshBaking/BakeLevelNav.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 
 public class BakeLevelNav : MonoBehaviour
 {
-    public NavMeshSurface surface;
-
     // Use this for initialization
     public void BuildNavigation()
     {
         //Even though this is simple, leaving as a seperate script in case I need to expand later
-        surface.BuildNavMesh();
+        foreach(NavMeshSurface surface in GetComponents<NavMeshSurface>())
+        {
+            surface.BuildNavMesh();
+        }
     }
 }
