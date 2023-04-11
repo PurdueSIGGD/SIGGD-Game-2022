150c3138eea4ba494fca9c221c990c698bd872fb
Jared Brown <57924789+JaredTheBro@users.noreply.github.com>
2023-04-10 20:20:53 -0400

    Various changes before merge

--- Assets/Scripts/RoomBudgeting.cs
+++ Assets/Scripts/RoomBudgeting.cs
@@ -3,11 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /* --- DEV NOTES ---
--   For now, we're assuming that there will be several set locations in a room where any object can spawn, so long as it isn't too
-large for that location. Eventually we can add functionality for spawning probability and constraints, but just assume that any
-object can spawn at the points for now.
--   STOPPING POINT: I believe the mins and maxes are working, and there is a safeguard that corrects it if the min is higher than the max. Next, I should focus on
-actually finding the spawnpoints in the room and actually placing the objects there before adding any more modifiers.
+    - NEXT: Fix an issue where the forced spawns aren't actually getting spawned.
 */
 
 /// <summary>
@@ -15,79 +11,91 @@ actually finding the spawnpoints in the room and actually placing the objects th
 /// </summary>
 public class RoomBudgeting : MonoBehaviour {
 
-    private List<GameObject> spawns;
     [SerializeField]
+    private List<Transform> spawns;
     
     // These lists should all be the same size! The index matters, because each index corresponds to one object. (Use a Map instead?)
+    [SerializeField]
     private List<Transform> spawnables = new List<Transform>();
     [SerializeField]
     private List<int> maximums = new List<int>(); // -1 indicates no maximum
     [SerializeField]
     private List<int> minimums = new List<int>();
+    [SerializeField]
+    private List<Transform> forcedSpawns = new List<Transform>();
 
     [SerializeField]
     private int budget = 0;
 
+    List<Transform> toSpawn = new List<Transform>();
     private List<int> numSpawned = new List<int>();
+    private int budgetLeft;
+    private int spawnsLeft;
+
 
     void Awake() {
         // Placeholder objects are stored in the Prefabs/RoomObjPlaceholders folder for now, with an Attributes script attached to each one
 
-        // Make all the serialized lists the same size as the spawnables list if their sizes don't match
-        correctListSizeInt(numSpawned, 0); // Needed here to make sure the list can store the number of each object spawned during the choosing process
-        correctListSizeInt(maximums, -1); // Again, -1 indicates no maximum
-        correctListSizeInt(minimums, 0);
-        correctMinAndMax(minimums, maximums);
+        budgetLeft = budget; // Used for spawning the objects because this instance will be decremented during that, while the original instance persists
 
-        int tempBudget = budget; // Used for spawning the objects because this instance will be deprecated during that, while the original instance persists
-        spawns = new List<GameObject>();
         Transform[] roomObjTransforms = transform.GetComponentsInChildren<Transform>(false);
         foreach (Transform t in roomObjTransforms) {
-            if (t.gameObject.tag.Equals("RoomSpawn")) {
-                spawns.Add(t.gameObject);
+            if (t.gameObject.tag.Equals("RoomSpawn") && !spawns.Contains(t)) {
+                print("A spawnpoint is being added to spawns.");
+                for (int i = 0; i <= spawns.Count; i++) {
+                    if (i == spawns.Count) {
+                        spawns.Add(t);
+                        break;
+                    }
+                    if (spawns[i] == null) {
+                        spawns[i] = t;
+                        break;
+                    }
+                }
+            }
+        }
+        spawnsLeft = spawns.Count; // Used to end the loop when this number reaches zero, being decremented for each object spawned
+
+        // Make all the serialized lists the same size as the spawnables list if their sizes don't match
+        correctListSizeIntSpawnables(numSpawned, 0); // Needed here to make sure the list can store the number of each object spawned during the choosing process
+        correctListSizeIntSpawnables(maximums, -1); // Again, -1 indicates no maximum
+        correctListSizeIntSpawnables(minimums, 0);
+        correctMinAndMax(minimums, maximums);
+        correctListSizeTransformSpawns(toSpawn, null); // Must do this so forced spawns can be inserted at desired indexes
+
+        // First, spawn all of the forced spawns -- objects that the user has attached to specific spawnpoints, saying that that
+        // specific object MUST spawn at THAT specific spawnpoint. Spawn these in first and then remove those spawnpoints from the
+        // "spawns" list, as they are no longer open spawnpoints. Keep in mind that the objects spawned here still count toward
+        // the budget.
+
+        for (int i = 0; i < forcedSpawns.Count; i++) {
+            if (forcedSpawns[i] != null) {
+                queueForcedSpawnIfAble(forcedSpawns[i], i);
             }
         }
 
-        // The objects' transforms to be allowed to spawn in the room should be added to the script's list through the serialized field in the editor.
-        List<Transform> toSpawn = new List<Transform>();
-
-        /* Version 3: Add the ability for devs to set constraints on how many objects can spawn in a room. Have a minimum and maximum
-        associated with that room through more lists serialized from this script, or making use of a Map-type list to pair the mins and
-        maxes with the objects that can be spawned. Also, have a global max and min that are attached to each object's attributes script,
-        and these values apply to every room the object spawns in. The most restrictive constraint between the local-to-room and global
-        ones is the one that applies (add an override option?). In addition, I might want to add a safeguard that makes sure all of the
-        arrays used for the objects are the same size, and if they aren't, average them and set default values. (Thinking I should use
-        the array with the actual objects to decide the sizes of the other ones). Then, after this, add actual spawn points in the room
-        and uncomment the part of the while loop conditional that stops the loop when an object has spawned in every available spawn point.
-        */
         for (int i = 0; i < spawnables.Count; i++) {
             if (maximums[i] == 0) {
                 removeFromPool(i);
                 i--;
             }
         }
-        while (spawnables.Count > 0 && toSpawn.Count < spawns.Count) {
-            for (int i = 0; i < spawnables.Count; i++) {
-                while (minimums[i] > numSpawned[i] && toSpawn.Count < spawns.Count) {
-                    if (spawnables[i].gameObject.GetComponent<Attributes>().weight <= tempBudget) {
-                        toSpawn.Add(spawnables[i]);
-                        tempBudget -= spawnables[i].gameObject.GetComponent<Attributes>().weight;
-                        numSpawned[i]++;
-                    } else {
-                        Debug.Log("Error: Can't meet the minimum spawns (" + minimums[i] + ") of " + spawnables[i].name + 
-                            " because it exceeds the room's remaining budget!");
-                        removeFromPool(i);
-                        i--;
-                        break;
-                    }
+
+        for (int i = 0; i < spawnables.Count; i++) {
+            while (minimums[i] > numSpawned[i] && spawnsLeft > 0) {
+                if (!queueSpawnIfAble(i)) {
+                    Debug.Log("Error: Can't meet the minimum spawns (" + minimums[i] + ") of " + spawnables[i].name + 
+                        " because it exceeds the room's remaining budget!");
+                    removeFromPool(i);
+                    i--;
+                    break;
                 }
             }
+        }
+
+        while (spawnables.Count > 0 && spawnsLeft > 0) {
             int chosenIndex = (int) Random.Range(0, spawnables.Count);
-            int objWeight = spawnables[chosenIndex].gameObject.GetComponent<Attributes>().weight;
-            if (objWeight <= tempBudget) {
-                toSpawn.Add(spawnables[chosenIndex]);
-                tempBudget -= objWeight;
-                numSpawned[chosenIndex]++;
+            if (queueSpawnIfAble(chosenIndex)) {
                 if (numSpawned[chosenIndex] >= maximums[chosenIndex] && maximums[chosenIndex] != -1) {
                     Debug.Log("Maximum spawns (" + maximums[chosenIndex] + ") reached for " + spawnables[chosenIndex].name + ".");
                     removeFromPool(chosenIndex);
@@ -96,13 +104,18 @@ public class RoomBudgeting : MonoBehaviour {
                 removeFromPool(chosenIndex);
             }
         }
+
+        // Debug stuff, remove later
+        // ***
         string test = "With a budget of " + budget + ", spawned these objects: ";
         foreach (Transform t in toSpawn) {
-            test += t.name + " (" + t.GetComponent<Attributes>().weight + "), ";
+            if (t != null) {
+                test += t.name + " (" + t.GetComponent<Attributes>().weight + "), ";
+            }
         }
-        test += "with " + tempBudget + " budget left over.";
-
+        test += "with " + budgetLeft + " budget left over.";
         Debug.Log(test);
+        // ***
 
         /* Original object weights:
             Easy Enemy - 6
@@ -124,17 +137,31 @@ public class RoomBudgeting : MonoBehaviour {
     /// and gives a warning that it was triggered.
     /// </summary>
     /// <param name="input"></param>
-    /// <returns>
-    /// The resized list
-    /// </returns>
-    private void correctListSizeInt(List<int> input, int valueToAdd) {
+    private void correctListSizeIntSpawnables(List<int> input, int valueToAdd) {
         while (input.Count < spawnables.Count) {
             input.Add(valueToAdd);
-            Debug.Log("Added an item to a modifier list to equalize its size with that of 'spawnables'.");
+            // Debug.Log("Added an item to a modifier list to equalize its size with that of 'spawnables'.");
         }
         while (input.Count > spawnables.Count) {
             input.RemoveAt(input.Count - 1);
-            Debug.Log("Removed an item from a modifier list to equalize its size with that of 'spawnables'.");
+            // Debug.Log("Removed an item from a modifier list to equalize its size with that of 'spawnables'.");
+        }
+    }
+
+    /// <summary>
+    /// Same function as correctListSizeInt, but adds objects instead of ints to correct the size, and uses the 'spawns' list as
+    /// the one with the correct size.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="objToAdd"></param>
+    private void correctListSizeTransformSpawns(List<Transform> input, Transform transToAdd) {
+        while (input.Count < spawns.Count) {
+            input.Add(transToAdd);
+            // Debug.Log("Added an item to a modifier list to equalize its size with that of 'spawnables'.");
+        }
+        while (input.Count > spawns.Count) {
+            input.RemoveAt(input.Count - 1);
+            // Debug.Log("Removed an item from a modifier list to equalize its size with that of 'spawnables'.");
         }
     }
 
@@ -145,7 +172,7 @@ public class RoomBudgeting : MonoBehaviour {
     /// <param name="maxes"></param>
     private void correctMinAndMax(List<int> mins, List<int> maxes) {
         for (int i = 0; i < spawnables.Count; i++) {
-            if (mins[i] > maxes[i]) { // This shouldn't happen. That's why we're correcting it if it does.
+            if ((mins[i] > maxes[i]) && (maxes[i] != -1)) { // This shouldn't happen. That's why we're correcting it if it does.
                 mins[i] = maxes[i]; // Sets the minimum value to the maximum value
             }
         }
@@ -170,34 +197,87 @@ public class RoomBudgeting : MonoBehaviour {
     /// </summary>
     /// <param name="toSpawn"></param>
     /// <param name="spawns"></param>
-    private void spawnObjects(List<Transform> toSpawn, List<GameObject> spawns) {
+    private void spawnObjects(List<Transform> toSpawn, List<Transform> spawns) {
         for (int i = 0; i < toSpawn.Count; i++) {
-            GameObject spawnedObj = GameObject.Instantiate(toSpawn[i].gameObject);
-            if (spawnedObj.transform.GetComponent<Collider>() is SphereCollider) {
-                spawnedObj.transform.position = new Vector3(
-                    spawns[i].transform.position.x,
-                    spawns[i].transform.position.y + (toSpawn[i].transform.localScale.y *
-                        (toSpawn[i].transform.GetComponent<SphereCollider>().radius)),
-                    spawns[i].transform.position.z);
-                print("Object spawned");
-            } else if (spawnedObj.transform.GetComponent<Collider>() is BoxCollider) {
-                spawnedObj.transform.position = new Vector3(
-                    spawns[i].transform.position.x,
-                    spawns[i].transform.position.y + (toSpawn[i].transform.localScale.y *
-                        (toSpawn[i].transform.GetComponent<BoxCollider>().size.y / 2)),
-                    spawns[i].transform.position.z);
-                print("Object spawned");
-            } else if (spawnedObj.transform.GetComponent<Collider>() is CapsuleCollider) {
-                spawnedObj.transform.position = new Vector3(
-                    spawns[i].transform.position.x,
-                    spawns[i].transform.position.y + (toSpawn[i].transform.localScale.y *
-                        (toSpawn[i].transform.GetComponent<CapsuleCollider>().height / 2)),
-                    spawns[i].transform.position.z);
-                print("Object spawned");
-            } else {
-                print("Error spawning object because its collider can't be handled.");
+            if (toSpawn[i] != null) {
+                GameObject spawnedObj = GameObject.Instantiate(toSpawn[i].gameObject);
+                if (spawnedObj.transform.GetComponent<Collider>() is SphereCollider) {
+                    spawnedObj.transform.position = new Vector3(
+                        spawns[i].position.x,
+                        spawns[i].position.y + (toSpawn[i].transform.localScale.y *
+                            (toSpawn[i].transform.GetComponent<SphereCollider>().radius)),
+                        spawns[i].position.z);
+                    print("Object spawned");
+                } else if (spawnedObj.transform.GetComponent<Collider>() is BoxCollider) {
+                    spawnedObj.transform.position = new Vector3(
+                        spawns[i].position.x,
+                        spawns[i].position.y + (toSpawn[i].transform.localScale.y *
+                            (toSpawn[i].transform.GetComponent<BoxCollider>().size.y / 2)),
+                        spawns[i].position.z);
+                    print("Object spawned");
+                } else if (spawnedObj.transform.GetComponent<Collider>() is CapsuleCollider) {
+                    spawnedObj.transform.position = new Vector3(
+                        spawns[i].position.x,
+                        spawns[i].position.y + (toSpawn[i].transform.localScale.y *
+                            (toSpawn[i].transform.GetComponent<CapsuleCollider>().height / 2)),
+                        spawns[i].position.z);
+                    print("Object spawned");
+                } else {
+                    spawnedObj.transform.position = new Vector3(
+                        spawns[i].position.x,
+                        spawns[i].position.y + (toSpawn[i].transform.localScale.y + 1),
+                        spawns[i].position.z);
+                    print("Object spawned...probably at the wrong height. Its collider can't yet be handled well, so go yell at"
+                        + " Jared to fix it.");
+                }
             }
         }
     }
-    
+
+    private bool queueSpawn(int objIndex, int spawnIndex) {
+
+        print("Attemtping to queue spawn of " + spawnables[objIndex].name + " at spawnpoint " + spawnIndex + ". There is "
+            + budgetLeft + " budget left before this attempt, and the object has a weight of "
+            + spawnables[objIndex].gameObject.GetComponent<Attributes>().weight + ".");
+
+        if (spawnables[objIndex].gameObject.GetComponent<Attributes>().weight <= budgetLeft) {
+            if (spawnIndex == -1) { // Index will be -1 if the spawn is random and not forced to be at a specific spawnpoint
+                for (int i = 0; i < toSpawn.Count; i++) {
+                    if (toSpawn[i] == null) {
+                        toSpawn[i] = spawnables[objIndex];
+                        budgetLeft -= spawnables[objIndex].gameObject.GetComponent<Attributes>().weight;
+                        numSpawned[objIndex]++;
+                        spawnsLeft--;
+                        return true;
+                    }
+                }
+            } else {
+                toSpawn[spawnIndex] = spawnables[objIndex];
+                budgetLeft -= spawnables[objIndex].gameObject.GetComponent<Attributes>().weight;
+                numSpawned[objIndex]++;
+                spawnsLeft--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool queueSpawnIfAble(int objIndex) {
+        return queueSpawn(objIndex, -1);
+    }
+
+    private bool queueForcedSpawnIfAble(int objIndex, int spawnIndex) {
+        return queueSpawn(objIndex, spawnIndex);
+    }
+
+    private bool queueForcedSpawnIfAble(Transform t, int spawnIndex) {
+        for (int i = 0; i < spawnables.Count; i++) {
+            if (spawnables[i] == t) {
+                Debug.Log("found");
+                return queueSpawn(i, spawnIndex);
+            }
+        }
+        return false;
+    }
+
 }
