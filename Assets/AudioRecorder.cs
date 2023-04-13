bebfa836bb87052958fa17cf93256dea6915e140
iyang2006 <yang2006@purdue.edu>
2023-04-12 20:32:55 -0400

    p

--- Assets/AudioRecorder.cs
+++ Assets/AudioRecorder.cs
@@ -4,8 +4,33 @@ using UnityEngine;
 
 public class AudioRecorder : Item
 {
+    [SerializeField] private AudioSource[] audioLogs;
+    [SerializeField] private AudioSource KGBLog1;
+    [SerializeField] private AudioSource KGBLog2;
+    [SerializeField] private AudioSource KGBLog3;
+    [SerializeField] private int useAudio;
+    private AudioSource audioToPlay;
+
     public override void Use()
     {
-        
+        // Based on the location, select one of the audio log files
+        if (useAudio == 1)
+        {
+            audioToPlay = KGBLog1;
+        }
+        else if (useAudio == 2)
+        {
+            audioToPlay = KGBLog2;
+        }
+        else if (useAudio == 3)
+        {
+            audioToPlay = KGBLog3;
+        }
+        else
+        {
+            audioToPlay = audioLogs[Random.Range(0, audioLogs.Length - 1)];
+        }
+
+        audioToPlay.Play();
     }
 }
