fab9ac082bfd15b1cbbcd7d3ca35558ea4335671
iyang2006 <yang2006@purdue.edu>
2023-04-28 00:23:44 -0400

    Final changes to scene movement and audio recorder item

--- Assets/Scripts/Interactables/Items/AudioRecorder.cs
+++ Assets/Scripts/Interactables/Items/AudioRecorder.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AudioRecorder : Item
 {
-    [SerializeField] private Item audioRecorder;
-    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip audioClip;
 
     public override void Use()
     {
         // Based on the location, select one of the audio log files
-
+        Transform playerTrans = (Transform)Variables.ActiveScene.Get("player");
+        AudioSource audioSource = playerTrans.GetComponent<AudioSource>();
+        audioSource.clip = audioClip;
         audioSource.Play();
     }
 }
